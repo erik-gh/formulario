cf4b3b6b2a700071a39b0cb3da1a25ce8daee501
erik-gh <eriktecse@gmail.com>
2022-06-30 03:29:06 -0500

    Empezando a crear con bootstrap

---
+++ app/Http/Livewire/SelectProcesoOdpe.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SelectProcesoOdpe extends Component
+{
+    public function render()
+    {
+        return view('livewire.select-proceso-odpe');
+    }
+}
